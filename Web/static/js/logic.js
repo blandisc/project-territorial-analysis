319dd9fcadfd9cd4b4577c4374f719bdd2508dc5
Fernando Iracheta <fer@MacBook-Pro-de-Fernando.local>
2020-07-21 21:04:36 -0500

    Map completed

---
+++ Web/static/js/logic.js
@@ -0,0 +1,14 @@
+// create variable mymap
+
+let mymap = L.map('map').setView([19.4296549,-99.2467057], 13);
+
+
+// Create the tile layer that will be the background of our map
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/light-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: apiKey
+}).addTo(mymap);
